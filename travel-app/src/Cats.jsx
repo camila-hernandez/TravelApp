7a3d6bec7069caf473cf66158915e3fccbd20e8b
Julie <JJSKIM@UCALGARY.CA>
2024-02-23 15:30:51 -0700

    Got React Working, but not showing on first page

---
+++ travel-app/src/Cats.jsx
@@ -0,0 +1,13 @@
+// Cats.js
+import React from 'react';
+import './Cats.css'; // Assuming you want separate CSS for this component
+
+const Cats = () => {
+  return (
+    <div className="cats-box">
+      I love cats
+    </div>
+  );
+};
+
+export default Cats;

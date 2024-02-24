05e12e1cca2b6ea16d4286c3687efc08cefcb883
Julie <JJSKIM@UCALGARY.CA>
2024-02-23 18:28:50 -0700

    Created MeowPage for Example
    Able to show

---
+++ travel-app/src/MeowPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="content-box">
+      <button className="meow-button">meow</button>
+    </div>
+  );
+}
+
+export default HomePage;

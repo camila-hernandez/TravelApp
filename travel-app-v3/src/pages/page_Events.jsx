3f33a1abf103d246d598ca4badb075de7fb2fc6a
Julie <JJSKIM@UCALGARY.CA>
2024-03-20 16:33:22 -0600

    Adding Event Pages (Not complete yet)

--- travel-app-v3/src/pages/page_Events.jsx
+++ travel-app-v3/src/pages/page_Events.jsx
@@ -1,14 +1,74 @@
-// pages/Page_Events.jsx
 import React from 'react';
-import '../App.css'; // Import your CSS file for styling
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faArrowDownShortWide} from "@fortawesome/free-solid-svg-icons";
+import "./page_Events.css";
 
-const Page_Events = () => {
+export default function Page_Events() {
   return (
     <div>
-      {/* Your home page content goes here */}
-      <h1>Welcome to Events Page</h1>
+      <div className="search">
+        <input type="text" className="searchTerm" placeholder="Search..." />
+        <button type="submit" className="searchButton">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="searchIcon" />
+        </button>
+      </div>
+      <div className="filter-button-container">
+        <button type="submit" className="filter-button">
+            Filter&nbsp;
+            <FontAwesomeIcon icon={faArrowDownShortWide} />
+        </button>
+      </div>
+      <div className="events-wrapper">
+        <div className="event-container">
+            <div className="event-date">
+                <div className="date-number">
+                    <h1>13</h1>
+                </div>
+                <div className="month">
+                    <h2>April</h2>
+                </div>
+            </div>
+            <div className="event-title">
+                <h1>Open Mic Event</h1>
+                <p>8:00pm - 8:30pm</p>
+                <p>Koi Bar | Calgary, AB</p>
+            </div>
+            <button className="details-button">View</button>
+        </div>
+        <div className="event-container">
+            <div className="event-date">
+                <div className="date-number">
+                    <h1>14</h1>
+                </div>
+                <div className="month">
+                    <h2>April</h2>
+                </div>
+            </div>
+            <div className="event-title">
+                <h1>Calgary Tower Tour</h1>
+                <p>2:00pm - 3:00pm</p>
+                <p>Calgary Tower | Calgary, AB</p>
+            </div>
+            <button className="details-button">View</button>
+        </div>
+        <div className="event-container">
+            <div className="event-date">
+                <div className="date-number">
+                    <h1>16</h1>
+                </div>
+                <div className="month">
+                    <h2>April</h2>
+                </div>
+            </div>
+            <div className="event-title">
+                <h1>Calgary Flames Game</h1>
+                <p>7:00pm - 10:00pm</p>
+                <p>Scotiabank Saddledome | Calgary, AB</p>
+            </div>
+            <button className="details-button">View</button>
+        </div>
+        {/* Repeat the event-container for each event */}
+      </div>
     </div>
   );
-};
-
-export default Page_Events;
+}
--- travel-app-v3/src/pages/page_Home.jsx
+++ travel-app-v3/src/pages/page_Home.jsx
@@ -1,13 +1,50 @@
-// pages/Page_Home.jsx
-import React from 'react';
-import '../App.css'; // Import your CSS file for styling
-const Page_Home = () => {
-  return (
-    <div>
-      {/* Your home page content goes here */}
-      <h1>Welcome to Home Page</h1>
-    </div>
-  );
-};
+import {useState} from "react";
+import whereLogo from '../assets/WhereLogoGreen.png';
+import weatherImage from '../assets/WeatherPic.png';
+import backgroundImage from '../assets/CalgaryBackground.png';
 
-export default Page_Home;
+export default function page_Home() {
+    const containerStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover', // Adjust as needed
+        backgroundPosition: 'center', // Adjust as needed
+        position: 'relative', // Required for z-index to work
+        height: '560px'
+    };
+    
+    const helpButtonStyle = {
+        width: '200px',
+        height: '50px',
+        fontSize: '20px',
+    };
+
+    const spaceBetweenStyle = {
+        margin: '15px', // Adjust the margin as needed
+      };
+
+    return (
+    <div class="helpPage-container" style = {containerStyle}> 
+
+        <div class="logo-field">
+            <img src={whereLogo} alt="whereLogo" className="whereLogo" 
+            style={{ width: '100px', height: 'auto' }}
+            />
+        </div>
+        <div class="weather-image-field">
+            <img src={weatherImage} alt="weatherImage" className="weatherImage" 
+            style={{ width: '350px', height: 'auto' }}
+            />
+        </div>
+        <div class="weather-button-field">
+            <button>
+                WeatherNetwork.ca
+            </button>
+        </div>
+        <div class="help-field" style={spaceBetweenStyle}>
+            <button style = {helpButtonStyle}>
+                Help
+            </button>
+        </div>
+    </div>
+    );
+}
